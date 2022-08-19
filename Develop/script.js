00254b602741207331852f2b03a0b86c1c9ee525
Kelsie Szost <kelsieszost@Kelsies-MacBook-Pro.local>
2022-08-18 21:30:02 -0400

    html/js redo

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,79 @@
-var digitalClock = function() {
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
 var todayDate = moment();
-$("#currentDay").text(todayDate.format('dddd MMM Do, YYYY, h:mm:ss'));
+
+
+
+
+
+
+function setPlanner() {
+  
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    setInterval(digitalClock, 1000);
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+
+        }
+
+    });
+
+}
+
+setPlanner();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
+
+var digitalClock = function () {
+    var todayDate = moment();
+
+
 
 };
 
 digitalClock();
 setInterval(digitalClock, 1000);
 
-$(document).ready(function() {
-    trackHours();
 
-    $("#hour9 .data-schedule").val(localStorage.getItem("hour9"));
-    $("#hour10 .data-schedule").val(localStorage.getItem("hour10"));
-    $("#hour11 .data-schedule").val(localStorage.getItem("hour11"));
-    $("#hour12 .data-schedule").val(localStorage.getItem("hour12"));
-    $("#hour13 .data-schedule").val(localStorage.getItem("hour13"));
-    $("#hour14 .data-schedule").val(localStorage.getItem("hour14"));
-    $("#hour15 .data-schedule").val(localStorage.getItem("hour15"));
-    $("#hour16 .data-schedule").val(localStorage.getItem("hour16"));
-    $("#hour17 .data-schedule").val(localStorage.getItem("hour17"));
-});
 
-$(".data-schedule").each (function() {
-    var id = $(this).attr("id");
-    var tasks = localStorage.getItem("id");
-    if(tasks !== null) {
-        $(this).children("tasks").val(tasks);
-    }
-})
 
-function trackHours() {
-    var currentTime = moment().hour();
 
-    $(".data-schedule").each(function() {
-        var calendarHour = parseInt($(this).attr("id"));
 
-        if(calendarHour === currentTime) {
-            $(this).addClass("present");
-
-        }else if (calendarHour < currentTime) {
-            $(this).addClass("past");
-
-        }else {
-            $(this).addClass("future");
-        }
-
-    })
-}
-
-var saveBtn = $(".data-save");
-
-saveBtn.on("click", function() {
-    var time =$(this).parent().attr("id");
-    var current = $(this).siblings(".schedule");
-    localStorage.setItem(time,current);
-
-});
+trackHours();
